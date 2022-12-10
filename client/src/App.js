c42b05bb95341dab70534cc26b644e7de0b6593d
Jacyked <services@jacykdev.com>
2022-12-10 00:13:59 -0700

    Reduce time for header text animation

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <AnimatedHeader sequence={["Hello", 700, "Hello World!", 2000, "I am Matt Jacyk, a Full Stack Developer."]} wrapper="h1" repeat='0'/>
+        <AnimatedHeader sequence={["Hello", 500, "Hello World!", 2000, "I am Matt Jacyk, a Full Stack Developer."]} wrapper="h1" repeat='0'/>
         <span>
           <a className="App-link" href="https://github.com/jacyked"><FontAwesomeIcon size="xl" icon={faGithub}/></a>
           <a className="App-link" href="https://www.linkedin.com/in/matt-jacyk-a13a8216b/"><FontAwesomeIcon size="xl" icon={faLinkedin}/></a>
